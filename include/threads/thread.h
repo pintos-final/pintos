7226ae43b99fb5381698142e508650c83aeae5f0
hdh853 <hdh853@gmail.com>
2025-11-29 20:17:24 +0900

    feat: 우선순위 스케줄링 구현 (#14)
    * style: synch.c 포멧 적용
    
    * feat: priority scheduling 구현
    - ready_list에 우선순위 고려해서 넣도록 수정
     - thread_create, set_thread_priority 시 우선순위 고려해서 yield 하도록 수정
    - 우선순위 비교 위한 priority_greater 함수 생성
    - 세마포어의 waiter리스트에 넣을 때도 우선순위 고려하도록 수정
    - cond_signal시 정렬 후 sema_up 하도록 수정
    
    * refactor: 우선순위 체크 방식 변경
    -  sema : 넣을 때 정렬해서 넣는 방식에서 빼낼 때 list_min으로 빼내는 방식으로 수정
    - cond : 빼낼때 sort 후 빼내던 방식에서 list_min으로 빼내는 방식으로 수정
    
    * refactor: sema_up에서 if문 분기 및 주석 추가
    
    * refactor: 잘못된 주석 수정 및 제거

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -145,4 +145,6 @@ int thread_get_load_avg(void);
 
 void do_iret(struct intr_frame* tf);
 
+bool priority_greater(const struct list_elem* a, const struct list_elem* b, void* aux UNUSED);
+
 #endif /* threads/thread.h */
