43a559918a38da99df53e19cb2fe296286311989
Hyun <itsmine096@gmail.com>
2025-12-03 00:03:55 +0900

    feat: phase1 시스템 콜 구현
    - phase1 시스템 콜 함수 구현 및 핸들러 연결
    - 프로세스별 파일 디스크립터 테이블(fd_table)과 FD_MAX, FD_MIN 상수 정의 추가
    - filesys_lock의 선언을 userprog/syscall.c에서 정의하고 헤더를 통해 공유하도록 수정

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -27,6 +27,10 @@ typedef int tid_t;
 #define PRI_DEFAULT 31 /* Default priority. */
 #define PRI_MAX 63     /* Highest priority. */
 
+/* File descriptor limit. */
+#define FD_MIN 2
+#define FD_MAX 128
+
 /* A kernel thread or user process.
  *
  * Each thread structure is stored in its own 4 kB page.  The
@@ -106,6 +110,8 @@ struct thread {
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint64_t* pml4; /* Page map level 4 */
+    struct file* fd_table[FD_MAX];
+    int exit_status;
 #endif
 #ifdef VM
     /* Table for whole virtual memory owned by thread. */
--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,9 +1,11 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include "filesys/filesys.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/loader.h"
+#include "threads/synch.h"
 #include "userprog/gdt.h"
 #include "threads/flags.h"
 #include "intrinsic.h"
@@ -24,10 +26,30 @@ void syscall_handler(struct intr_frame*);
 #define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
 
+#define EXIT_FAILURE -1    /* 프로세스 종료 코드 */
+#define SYSCALL_FAILURE -1 /* 시스템콜 반환값 */
+
+#define STDIN_FILENO 0
+#define STDOUT_FILENO 1
+
+struct lock filesys_lock;
+
 static void check_valid_user_pointer(const void* uaddr);
 static void check_writable_pointer(void* uaddr);
 static void check_valid_buffer(const void* uaddr, size_t size, bool check_write);
 static void check_valid_string(const char* str);
+
+static void halt(void) NO_RETURN;
+static void exit(int status) NO_RETURN;
+static bool create(const char* file, unsigned initial_size);
+static bool remove(const char* file);
+static int open(const char* file);
+static int filesize(int fd);
+static int read(int fd, void* buffer, unsigned length);
+static int write(int fd, const void* buffer, unsigned length);
+static void seek(int fd, unsigned position);
+static unsigned tell(int fd);
+static void close(int fd);
 
 void syscall_init(void)
 {
@@ -38,14 +60,50 @@ void syscall_init(void)
      * until the syscall_entry swaps the userland stack to the kernel
      * mode stack. Therefore, we masked the FLAG_FL. */
     write_msr(MSR_SYSCALL_MASK, FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+
+    lock_init(&filesys_lock);
 }
 
 /* The main system call interface */
 void syscall_handler(struct intr_frame* f UNUSED)
 {
-    // TODO: Your implementation goes here.
-    printf("system call!\n");
-    thread_exit();
+    switch (f->R.rax) {
+    case SYS_HALT:
+        halt();
+        break;
+    case SYS_EXIT:
+        exit((int)f->R.rdi);
+        break;
+    case SYS_CREATE:
+        f->R.rax = create((const char*)f->R.rdi, (unsigned)f->R.rsi);
+        break;
+    case SYS_REMOVE:
+        f->R.rax = remove((const char*)f->R.rdi);
+        break;
+    case SYS_OPEN:
+        f->R.rax = open((const char*)f->R.rdi);
+        break;
+    case SYS_FILESIZE:
+        f->R.rax = filesize((int)f->R.rdi);
+        break;
+    case SYS_READ:
+        f->R.rax = read((int)f->R.rdi, (void*)f->R.rsi, (unsigned)f->R.rdx);
+        break;
+    case SYS_WRITE:
+        f->R.rax = write((int)f->R.rdi, (const void*)f->R.rsi, (unsigned)f->R.rdx);
+        break;
+    case SYS_SEEK:
+        seek((int)f->R.rdi, (unsigned)f->R.rsi);
+        break;
+    case SYS_TELL:
+        f->R.rax = tell((int)f->R.rdi);
+        break;
+    case SYS_CLOSE:
+        close((int)f->R.rdi);
+        break;
+    default:
+        exit(EXIT_FAILURE);
+    }
 }
 
 /* 유저 포인터가 유효한지 검사하고, 잘못된 경우 프로세스를 종료
@@ -121,4 +179,179 @@ static void check_valid_string(const char* str)
             return;
     }
 }
+
+static struct file* get_file_from_fd(int fd)
+{
+    if (fd < FD_MIN || fd >= FD_MAX)
+        return NULL;
+    return thread_current()->fd_table[fd];
+}
+
+static void halt(void)
+{
+    power_off();
+    NOT_REACHED();
+}
+
+static void exit(int status)
+{
+    thread_current()->exit_status = status;
+    thread_exit();
+    NOT_REACHED();
+}
+
+static bool create(const char* file, unsigned initial_size)
+{
+    check_valid_string(file);
+
+    lock_acquire(&filesys_lock);
+    bool is_success = filesys_create(file, initial_size);
+    lock_release(&filesys_lock);
+
+    return is_success;
+}
+
+static bool remove(const char* file)
+{
+    check_valid_string(file);
+
+    lock_acquire(&filesys_lock);
+    bool is_success = filesys_remove(file);
+    lock_release(&filesys_lock);
+
+    return is_success;
+}
+
+static int open(const char* file)
+{
+    check_valid_string(file);
+
+    lock_acquire(&filesys_lock);
+    struct file* opened_file = filesys_open(file);
+    lock_release(&filesys_lock);
+
+    if (opened_file == NULL) {
+        return SYSCALL_FAILURE;
+    }
+
+    struct thread* curr = thread_current();
+    for (int fd = FD_MIN; fd < FD_MAX; fd++) {
+        if (curr->fd_table[fd] == NULL) {
+            curr->fd_table[fd] = opened_file;
+            return fd;
+        }
+    }
+
+    // FD 테이블이 가득 찬 경우
+    lock_acquire(&filesys_lock);
+    file_close(opened_file);
+    lock_release(&filesys_lock);
+    return SYSCALL_FAILURE;
+}
+
+static int filesize(int fd)
+{
+
+    struct file* f = get_file_from_fd(fd);
+    if (f == NULL) {
+        return SYSCALL_FAILURE;
+    }
+
+    lock_acquire(&filesys_lock);
+    int size = file_length(f);
+    lock_release(&filesys_lock);
+    return size;
+}
+
+static int read(int fd, void* buffer, unsigned length)
+{
+    if (length == 0)
+        return 0;
+
+    check_valid_buffer(buffer, length, true);
+
+    if (fd == STDIN_FILENO) {
+        char* buf = buffer;
+        for (unsigned i = 0; i < length; i++)
+            buf[i] = input_getc();
+        return length;
+    }
+
+    if (fd == STDOUT_FILENO)
+        return SYSCALL_FAILURE;
+
+    struct file* f = get_file_from_fd(fd);
+    if (f == NULL)
+        return SYSCALL_FAILURE;
+
+    lock_acquire(&filesys_lock);
+    int read_bytes = file_read(f, buffer, length);
+    lock_release(&filesys_lock);
+
+    return read_bytes;
+}
+
+static int write(int fd, const void* buffer, unsigned length)
+{
+    if (length == 0)
+        return 0;
+
+    check_valid_buffer(buffer, length, false);
+
+    if (fd == STDIN_FILENO)
+        return SYSCALL_FAILURE;
+
+    if (fd == STDOUT_FILENO) {
+        putbuf(buffer, length);
+        return length;
+    }
+
+    struct file* f = get_file_from_fd(fd);
+    if (f == NULL)
+        return SYSCALL_FAILURE;
+
+    lock_acquire(&filesys_lock);
+    int written_bytes = file_write(f, buffer, length);
+    lock_release(&filesys_lock);
+
+    return written_bytes;
+}
+
+static void seek(int fd, unsigned position)
+{
+    struct file* f = get_file_from_fd(fd);
+    if (f == NULL) {
+        return;
+    }
+
+    lock_acquire(&filesys_lock);
+    file_seek(f, position);
+    lock_release(&filesys_lock);
+}
+
+static unsigned tell(int fd)
+{
+    struct file* f = get_file_from_fd(fd);
+    if (f == NULL) {
+        return SYSCALL_FAILURE;
+    }
+
+    lock_acquire(&filesys_lock);
+    unsigned pos = file_tell(f);
+    lock_release(&filesys_lock);
+    return pos;
+}
+
+static void close(int fd)
+{
+    struct file* f = get_file_from_fd(fd);
+    if (f == NULL) {
+        return;
+    }
+
+    lock_acquire(&filesys_lock);
+    file_close(f);
+    lock_release(&filesys_lock);
+
+    thread_current()->fd_table[fd] = NULL;
 }
